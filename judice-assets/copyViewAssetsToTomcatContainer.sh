6dff034c12af98c7f00c19b671130827eb5ba42d
arilsonhub <arilson.senac.ads@gmail.com>
2019-02-14 13:06:33 -0200

    Update copyViewAssetsToTomcatContainer.sh

--- judice-assets/copyViewAssetsToTomcatContainer.sh
+++ judice-assets/copyViewAssetsToTomcatContainer.sh
@@ -2,12 +2,15 @@
 
 homePath="home/arilson"
 workspacePath="eclipse-workspace"
-projectPath="JudiceWeb_2.7"
+projectPath=$1
 contextFolderPath="/$homePath/$workspacePath/$projectPath/context/"
+containerName="docker_tomcat_1"
+containerContextPath="/usr/local/tomcat/webapps/judice"
 
 cd /
 cd $contextFolderPath
-docker cp module/. docker_tomcat_1:/usr/local/tomcat/webapps/judice/module/
-docker cp script/. docker_tomcat_1:/usr/local/tomcat/webapps/judice/script/
-docker cp style/. docker_tomcat_1:/usr/local/tomcat/webapps/judice/style/
+docker cp main/. $containerName:$containerContextPath/main/
+docker cp module/. $containerName:$containerContextPath/module/
+docker cp script/. $containerName:$containerContextPath/script/
+docker cp style/. $containerName:$containerContextPath/style/
 echo "Os arquivos foram copiados para o container."
